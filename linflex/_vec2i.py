85ebea02432b438d3c840d44589c2147c7618617
Havsalt <knutolaihelgesen@gmail.com>
2025-02-13 18:08:43 +0100

    Formatted

--- linflex/_vec2i.py
+++ linflex/_vec2i.py
@@ -1,4 +1,4 @@
-from __future__ import annotations as _annotations
+from __future__ import annotations
 
 from ._vec2 import Vec2
 
@@ -10,6 +10,7 @@ class Vec2i(Vec2):
 
     Usefull for storing whole numbers in 2D space
     """
+
     __slots__ = ("x", "y")
 
     def __init__(self, x: int = 0, y: int = 0, /) -> None:
@@ -21,106 +22,104 @@ class Vec2i(Vec2):
         """
         self.x = x
         self.y = y
-    
-    def __abs__(self) -> Vec2:
-        return Vec2(abs(self.x), abs(self.y))
-    
-    def __neg__(self) -> Vec2i:
-        return Vec2i(-self.x, -self.y)
 
     def __add__(self, other: Vec2i | Vec2) -> Vec2i | Vec2:
         if isinstance(other, Vec2i):
-            return Vec2i(int(self.x + other.x),
-                         int(self.y + other.y))
-        return Vec2(self.x + other.x,
-                    self.y + other.y)
-    
+            return Vec2i(int(self.x + other.x), int(self.y + other.y))
+        return Vec2(self.x + other.x, self.y + other.y)
+
     def __iadd__(self, other: Vec2i) -> Vec2i:
         self.x += other.x
         self.y += other.y
         return self
-    
+
     def __sub__(self, other: Vec2i | Vec2) -> Vec2i | Vec2:
         if isinstance(other, Vec2i):
-            return Vec2i(int(self.x - other.x),
-                         int(self.y - other.y))
-        return Vec2(self.x - other.x,
-                    self.y - other.y)
-    
+            return Vec2i(int(self.x - other.x), int(self.y - other.y))
+        return Vec2(self.x - other.x, self.y - other.y)
+
     def __isub__(self, other: Vec2i) -> Vec2i:
         self.x -= other.x
         self.y -= other.y
         return self
-    
+
     def __mul__(self, other: Vec2i | Vec2 | int | float) -> Vec2i | Vec2:
         if isinstance(other, Vec2i):
-            return Vec2i(int(self.x * other.x),
-                         int(self.y * other.y))
+            return Vec2i(int(self.x * other.x), int(self.y * other.y))
         elif isinstance(other, Vec2):
-            return Vec2(self.x * other.x,
-                        self.y * other.y)
-        return Vec2(self.x * other,
-                    self.y * other)
-    
+            return Vec2(self.x * other.x, self.y * other.y)
+        return Vec2(self.x * other, self.y * other)
+
     def __imul__(self, other: Vec2i) -> Vec2i:
         self.x *= other.x
         self.y *= other.y
         return self
-    
+
     def __floordiv__(self, other: Vec2i | Vec2 | int | float) -> Vec2i | Vec2:
         if isinstance(other, Vec2i):
-            return Vec2i(self.x // other.x,
-                         self.y // other.y)
+            return Vec2i(
+                self.x // other.x,
+                self.y // other.y,
+            )
         elif isinstance(other, Vec2):
-            return Vec2(self.x // other.x,
-                        self.y // other.y)
+            return Vec2(
+                self.x // other.x,
+                self.y // other.y,
+            )
         elif isinstance(other, int):
-            return Vec2i(self.x // other,
-                         self.y // other)
-        return Vec2(self.x // other,
-                    self.y // other)
-    
+            return Vec2i(
+                self.x // other,
+                self.y // other,
+            )
+        return Vec2(
+            self.x // other,
+            self.y // other,
+        )
+
     def __ifloordiv__(self, other: Vec2i) -> Vec2i:
         self.x //= other.x
         self.y //= other.y
         return self
-    
+
     def __truediv__(self, other: Vec2i | Vec2 | int | float) -> Vec2i | Vec2:
         if isinstance(other, Vec2i):
-            return Vec2i(int(self.x / other.x),
-                         int(self.y / other.y))
+            return Vec2i(
+                int(self.x / other.x),
+                int(self.y / other.y),
+            )
         elif isinstance(other, Vec2):
-            return Vec2(self.x / other.x,
-                        self.y / other.y)
-        return Vec2(self.x / other,
-                    self.y / other)
-    
+            return Vec2(
+                self.x / other.x,
+                self.y / other.y,
+            )
+        return Vec2(
+            self.x / other,
+            self.y / other,
+        )
+
     def __itruediv__(self, other: Vec2i) -> Vec2i:
         self.x /= other.x
         self.y /= other.y
         return self
-    
+
     def __mod__(self, other: Vec2i | Vec2 | int | float) -> Vec2i | Vec2:
         if isinstance(other, Vec2i):
-            return Vec2i(int(self.x % other.x),
-                         int(self.y % other.y))
+            return Vec2i(int(self.x % other.x), int(self.y % other.y))
         elif isinstance(other, Vec2):
-            return Vec2(self.x % other.x,
-                        self.y % other.y)
-        return Vec2(self.x % other,
-                    self.y % other)
-    
+            return Vec2(self.x % other.x, self.y % other.y)
+        return Vec2(self.x % other, self.y % other)
+
     def __imod__(self, other: Vec2i) -> Vec2i:
         self.x %= other.x
         self.y %= other.y
         return self
-    
+
     def __copy__(self) -> Vec2i:
         return __class__(self.x, self.y)
-    
+
     def __deepcopy__(self) -> Vec2i:
         return __class__(self.x, self.y)
-    
+
     def to_tuple(self) -> tuple[int, int]:
         """Converts the vector to a tuple representation
 
@@ -128,7 +127,7 @@ class Vec2i(Vec2):
             tuple[int, int]: x and y as tuple
         """
         return (self.x, self.y)
-    
+
     def copy(self) -> Vec2i:
         """Returns a copied Vec2i
 
