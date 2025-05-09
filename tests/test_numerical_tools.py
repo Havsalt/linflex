e40a836786bc7b5f0e2295547044f0bed727fd03
Havsalt <77575424+Havsalt@users.noreply.github.com>
2025-05-09 18:08:32 +0200

    Added testing of move_toward

--- tests/test_numerical_tools.py
+++ tests/test_numerical_tools.py
@@ -1,4 +1,4 @@
-from linflex import clamp, sign, lerp
+from linflex import clamp, sign, lerp, move_toward
 
 
 def test_clamp():
@@ -26,3 +26,15 @@ def test_lerp():
     assert lerp(0, 1, 0.50) == 0.5
     assert lerp(-10, -20, 0.50) == -15
     assert lerp(20, -20, 0.50) == 0
+
+
+def test_move_toward():
+    assert move_toward(0, 10, 1) == 1
+    assert move_toward(0, 10, 10) == 10
+    assert move_toward(0, 10, 15) == 10
+    assert move_toward(10, 0, 1) == 9
+    assert move_toward(10, 0, 10) == 0
+    assert move_toward(10, 0, 15) == 0
+    assert move_toward(5, 5, 1) == 5
+    assert move_toward(5.0, 10.0, 2.5) == 7.5
+    assert move_toward(10.0, 5.0, 2.5) == 7.5
